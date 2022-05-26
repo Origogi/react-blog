bf06df3013f763ab6e2f2c480dd1b89634185b60
tony.origogi <tony.origogi@kakaobrain.com>
2022-05-26 11:02:25 +0900

    test React useState

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  let post = '강남 우동 맛집';
+  let [title1, setTitle1] = useState('남자 코트 추천');
+  let [title2, setTitle2] = useState('강남 우동 맛집');
+  let [title3, setTitle3] = useState('파이썬 독학');
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="black-nav">
+        <h4 style={{ color: 'red', fontSize: '20px' }}>블로그임</h4>
+      </div>
+
+      <div className="list">
+        <h4>{title1}</h4>
+        <p>2월 17일 발행</p>
+      </div>
+      <div className="list">
+        <h4>{title2}</h4>
+        <p>2월 17일 발행</p>
+      </div>
+      <div className="list">
+        <h4>{title3}</h4>
+        <p>2월 17일 발행</p>
+      </div>
     </div>
   );
 }
