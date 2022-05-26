970f954e636e7fdd942e3c40e0fa15a4a224ee12
Origogi <rlawjdxo1212@naver.com>
2022-05-26 20:47:05 +0900

    정렬 버튼 추가

--- src/App.js
+++ src/App.js
@@ -1,35 +1,35 @@
-import { useState } from 'react';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
   let [titles, setTitles] = useState([
-    '남자 코트 추천',
-    '강남 우동 맛집',
-    '파이썬 독학',
+    "남자 코트 추천",
+    "강남 우동 맛집",
+    "파이썬 독학",
   ]);
 
   let [like, setLike] = useState(0);
 
   function onClick() {
-    console.log('1');
+    console.log("1");
   }
 
   return (
     <div className="App">
       <div className="black-nav">
-        <h4 style={{ color: 'red', fontSize: '20px' }}>블로그임</h4>
+        <h4 style={{ color: "red", fontSize: "20px" }}>블로그임</h4>
       </div>
 
       <div className="list">
         <h4>
-          {titles[0]}{' '}
+          {titles[0]}{" "}
           <span
             onClick={() => {
               setLike(like + 1);
             }}
           >
             👍
-          </span>{' '}
+          </span>{" "}
           {like}
         </h4>
         <p>2월 17일 발행</p>
@@ -42,9 +42,21 @@ function App() {
         <h4>{titles[2]}</h4>
         <p>2월 17일 발행</p>
       </div>
+
       <button
         onClick={() => {
-          setTitles(['여자 코트 추천', titles[1], titles[2]]);
+          let result = [...titles];
+          result.sort();
+          setTitles(result);
+        }}
+      >
+        가나다순정렬
+      </button>
+      <button
+        onClick={() => {
+          let result = [...titles];
+          result[0] = "여자 코트 추천";
+          setTitles(result);
         }}
       >
         타이틀 변경
